865eb1e97fe23db5c983f8a6eea870572addd2d3
FelixOe <felix90@ufl.edu>
2017-09-24 00:41:00 +0200

    Added syling and layout

---
+++ Website/UFEvaluations/UFEvaluations/Controllers/CollegesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UFEvaluations.Controllers
+{
+    public class CollegesController : Controller
+    {
+        // GET: Colleges
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Website/UFEvaluations/UFEvaluations/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UFEvaluations.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Departments
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
